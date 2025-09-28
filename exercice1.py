994a4abe6c992003e65958169c969143ba8c6901
Kedrebeogo Deogratias <Kedrebeogodeogratias@aol.com>
2025-09-29 00:23:13 +0200

    correct exo 1

--- exercice1.py
+++ exercice1.py
@@ -1,7 +1,7 @@
 import random
 
 def gen_list_random_int(int_nbr=10,int_binf=0,int_bsup=10):
-    
+
         l=[]
         for i in range(int_nbr):
             l.append(random.randint(int_binf,int_bsup-1))
